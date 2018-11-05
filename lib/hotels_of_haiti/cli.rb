8eb52cff7cf8bd01d53ba5b183a4d149cb7edbc5
GilTorch <gilbert.torchon@esih.edu>
2018-11-05 11:58:04 -0500

    basic cli methods created. will fill them later

--- lib/hotels_of_haiti/cli.rb
+++ lib/hotels_of_haiti/cli.rb
@@ -1,5 +1,22 @@
 class HotelsOfHaiti::CLI 
     def call 
-        puts "Hello from CLI class"
+        puts "Welcome to 'Hotels of Haiti'. Choose a city to see its list of hotels."
+        list_cities
+        menu
+    end
+
+    def list_cities 
+        puts "1. Cap-Haitien - 38 hotels"
+        puts "2. Gonaives - 3 hotels"
+        puts "3. Cayes - 4 hotels "
+    end
+
+    def list_hotels_in_city(city)
+    end
+
+    def menu 
+       puts "Enter your choice (number or name of city)"
+       input=gets.strip 
+
     end
 end
